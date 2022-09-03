2451035df2b974932838be0e43aa8aab25401550
RoshanDiwedi <sroshan154@gmail.com>
2022-09-03 12:40:12 +0530

    header style

--- script.js
+++ script.js
@@ -26,4 +26,21 @@ $(document).ready( function () {
        onInit: function (slick) {
        }
    });
+
+   const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navMenu.css("display","none")
+  })
+);
+
  });
