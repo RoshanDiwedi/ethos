177b44dd7f0f084fb1c6d50e23e8c8391b291719
roshan <67923969+RoshanDiwedi@users.noreply.github.com>
2022-09-04 21:52:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready( function () {
+ 
+    //// Slider Top
+   $('.js-items-slider-container .slider-for').slick({
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       arrows: false,
+       fade: true,
+       asNavFor: '.js-items-slider-container .slider-nav'
+   });
+
+   $('.js-items-slider-container .slider-nav').slick({
+       slidesToShow: 4,
+       slidesToScroll: 1,
+       asNavFor: '.js-items-slider-container .slider-for',
+       dots: true,
+       centerMode: false,
+       focusOnSelect: false,
+       arrows: true,
+       accessibility: true,
+       onAfterChange: function (slide, index) {
+         console.log("slider-nav change");
+         console.log(this.$slides.get(index));
+       },
+       onInit: function (slick) {
+       }
+    });
+
+    const hamburger = document.querySelector(".hambarger");
+    const navMenu = document.querySelector(".navbar-wrapper");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+      navMenu.classList.toggle("navbar-active");
+    });
+
+    document.querySelectorAll(".nav-link").forEach((link) =>
+      link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        navMenu.css("display","none")
+      })
+    );
+
+
+  // footer ul hover effect
+  $(".footer .top-footer ul li").on("mouseover", function(){
+    $(this).addClass("active");
+  });
+
+  $(".footer .top-footer ul li").on("mouseout", function(){
+    $(this).removeClass("active");
+  });
+
+ });
