56ad8b048c8dcac0c67fb93c89a9b5dee3ce9890
RoshanDiwedi <sroshan154@gmail.com>
2022-09-16 21:42:11 +0530

    sdvfsfsdfsd

--- microsoft/script.js
+++ microsoft/script.js
@@ -1,15 +1,41 @@
 $(document).ready(function(){
 
-    // Slick slider
-    $(".slick-slider").slick({
-        slidesToShow: 3,
-        infinite:false,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-          // dots: false, Boolean
-         // arrows: false, Boolean
-       });
+      // mobile navigation
+      $(".mobile-bar .fa-bars").on("click", function(){
+        $(".navbar-top").show();
+        $(this).hide();
+        $(".fa-xmark").show();
+      });
+
+      $(".mobile-bar .fa-xmark").on("click", function(){
+        $(".navbar-top").hide();
+        $(this).hide();
+        $(".fa-bars").show();
+      });
+
+
+      // contact section
+      $(".video-icon span").on("click", function(){
+        $(".contact .video").slideDown();
+      })
+      $(".video .fa-xmark").on("click", function(){
+        $(".contact .video").slideUp();
+      })
+
+
+
+    //       // Slick slider
+    // $(".slick-slider").slick({
+    //   slidesToShow: 3,
+    //   infinite:false,
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000
+    //     // dots: false, Boolean
+    //    // arrows: false, Boolean
+    // });
+      
+       
 });
 
 
